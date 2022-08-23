60f89e34a00c8543e0396f6e2be84a6d66164e51
Sanny Taron <sanny666@office.nfe.go.th>
2022-08-23 15:41:12 +0700

    Add adminLTE backend dashbord

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\BackendController;
 
 Route::get('/',[HomeController::class,'home']);
 Route::get('login',[HomeController::class,'login']);
@@ -9,6 +10,5 @@ Route::get('about',[HomeController::class,'about']);
 Route::get('service',[HomeController::class,'service']);
 Route::get('contact',[HomeController::class,'contact']);
 
-route::get('product',function(){
-    return "Product Pages";
-});
+//backend route
+Route::get('backend/dashboard',[BackendController::class,'dashboard']);
